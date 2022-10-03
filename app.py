18c9bb57bed37953e308e5fe91079f4307379541
Helen Root <helenroot1@gmail.com>
2022-10-03 14:22:46 +0200

    Ajouter des exemples de PATCH et POST; styling

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, url_for, render_template, session, redirect
+from flask import Flask
+from flask import url_for, render_template, redirect
+from flask import session, request
 from authlib.integrations.flask_client import OAuth
 import os
 
@@ -56,23 +58,79 @@ def get_token(session):
         )
 
 
-@app.route('/profile')
+@app.route('/profile', methods=['GET'])
 def profile():
     token = get_token(session)
     if token:
+        # get user info
         resp = oauth.ma_cantine.get('api/v1/userInfo/', token=token)
-        resp.raise_for_status()
         profile = resp.json()
+
+        # get canteen previews
         resp = oauth.ma_cantine.get('api/v1/canteenPreviews/', token=token)
-        resp.raise_for_status()
         previews = resp.json()
-        return render_template('profile.html', profile=profile, previews=previews)
+
+        # get first canteen and last year's diagnostic
+        canteen = None
+        year = 2021
+        diagnostic = None
+        if previews[0]:
+            resp = oauth.ma_cantine.get(f"api/v1/canteens/{ previews[0]['id'] }", token=token)
+            canteen = resp.json()
+            # a diagnostic for last year might not already exist
+            diagnostic = next(filter(lambda d: d['year'] == year, canteen['diagnostics']), None)
+
+        return render_template('profile.html',
+            profile=profile,
+            previews=previews,
+            canteen=canteen,
+            diagnostic=diagnostic,
+            year=year,
+            status=request.args.get('status'),
+            error=request.args.get('error')
+        )
     else:
         return redirect('/')
 
+@app.route('/profile', methods=['POST'])
+def update_profile():
+    token = get_token(session)
+    if token:
+        canteen_id = request.form['canteen_id']
+        # update canteen
+        resp = oauth.ma_cantine.patch(
+            f"api/v1/canteens/{ canteen_id }",
+            token=token,
+            data={ 'name': request.form['canteen_name'] }
+        )
+        if resp.status_code < 300:
+            # if canteen update was successful, continue to diagnostic create/update
+            total = request.form['total']
+            diagnostic_id = request.form.get('diagnostic_id', None)
+            if diagnostic_id:
+                # update diagnostic
+                resp = oauth.ma_cantine.patch(
+                    f"api/v1/canteens/{ canteen_id }/diagnostics/{ diagnostic_id }",
+                    token=token,
+                    # NB: you can provide the key in camelCase or snake_case
+                    data={ 'value_total_ht': total }
+                )
+            elif total:
+                # create diagnostic
+                resp = oauth.ma_cantine.post(
+                    f"api/v1/canteens/{ canteen_id }/diagnostics/",
+                    token=token,
+                    data={ 'valueTotalHt': total, 'year': request.form['year'] }
+                )
 
-# POST
-
+        # update params with info of last API call
+        params = f'?status={resp.status_code}'
+        if resp.status_code >= 300:
+            error = f'{resp.text}'
+            params += f'&error={error}'
+        return redirect(f'/profile{params}')
+    else:
+        return redirect('/')
 
 @app.route('/logout')
 def logout():
